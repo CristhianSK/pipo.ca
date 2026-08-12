25310c36a7db7f88d0a7d04099734f0f975d22a3
Cristhian <cristhian.rezende@rpisolucoes.com.br>
2026-08-12 14:01:27 -0300

    feat: setup initial project structure and configs

---
+++ src/main/java/com/pipoca/PipocaApplication.java
@@ -0,0 +1,12 @@
+package com.pipoca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipocaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PipocaApplication.class, args);
+    }
+}
---
+++ src/test/java/com/pipoca/PipocaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pipoca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipocaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
